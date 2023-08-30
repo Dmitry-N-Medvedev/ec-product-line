12552977c416659c32e1c045a815bd2dc79d41cb
Dmitry Medvedev <dmitry.medvedev@gmail.com>
2023-08-30 19:41:29 +0200

    added a bit more of the configurations

---
+++ sources/front-end/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import 'inter-ui/inter.css';
+	import { env } from '$env/dynamic/public';
+	import { browser as IsInBrowser } from '$app/environment';
+	import { onMount, onDestroy } from 'svelte';
+
+	onMount(async () => {
+		if (IsInBrowser === true) {
+			console.log('onMount');
+		}
+	});
+
+	onDestroy(async () => {
+		if (IsInBrowser === true) {
+			console.log('onDestroy');
+		}
+	});
+</script>
+
+<article>
+	<slot />
+</article>
